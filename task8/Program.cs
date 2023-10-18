4c71a227d2e6a4ba3d40c768547c9859fa6f32d2
Anastasia <nastya.lavrenteva.1996@mail.ru>
2023-10-19 02:36:22 +0300

    ДЗ

---
+++ task8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Answer {
+    static void PrintEvenNumbers(int number)
+    {
+      // Введите свое решение ниже
+      int i=2;
+while (number>=i)
+{
+  Console.Write($"{i}\t");
+  
+  i=i+2;
+}
+      
+    }
+
+
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 11;
+        }
+
+        // Не удаляйте строки ниже
+        PrintEvenNumbers(number);
+    }
